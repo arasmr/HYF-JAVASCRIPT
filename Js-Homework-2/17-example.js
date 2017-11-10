7528c6ae8b878988871372acbd067a5f1dc9a0e5
Emre DURSUN <32521540+infoemredursun@users.noreply.github.com>
2017-11-10 19:52:13 +0100

    Add files via upload

---
+++ Js-Homework-2/17-example.js
@@ -0,0 +1,8 @@
+// 17.Take a look at the following code:
+let o1 = { foo: 'bar' };
+let o2 = { foo: 'bar' };
+let o3 = o2;
+
+// Show that changing o2 changes o3 (or not) and changing o2 changes o3 o1 changes o3(or not).
+
+// Does the order that you assign (o3 = o2 or o2 = o3) matter? {Jim Cramer: ???}
